8038e51bbd8f8bb961087c509114073ca1a13196
mariomayorga2010 <mariomayorga2010@gmail.com>
2019-05-26 21:39:20 -0500

    cambios tizara

--- enviar.php
+++ enviar.php
@@ -19,7 +19,7 @@ $mensaje .= "Mensaje: " . $mensaje . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
 $para = 'mariomayorga2010@gmail.com';
-$asunto = 'Mensaje de mi sitio web';
+$asunto = 'Mensaje de tizara.mx';
 
 if (mail($para, $asunto, utf8_decode($mensaje), $header))
 echo "<script type='text/javascript'>alert('Tu mensaje ha sido enviado exitosamente');</script>";
